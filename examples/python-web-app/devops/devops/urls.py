47f197746d2592cab63f02d7d2dca504c97d095e
samidha <samidhawani7@gmail.com>
2025-10-30 19:31:51 +0530

    Update urls.py

--- examples/python-web-app/devops/devops/urls.py
+++ examples/python-web-app/devops/devops/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/demo/')),
+    path('', home),
     path('demo/', include('demo.urls')),
     path('admin/', admin.site.urls),
 ]
